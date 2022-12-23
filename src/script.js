95e7c3631cb2725012f447f9660c7f516d4b6f91
Maxim <118841284+MaximA21@users.noreply.github.com>
2022-12-23 16:03:00 +0100

    analyse

--- src/script.js
+++ src/script.js
@@ -2,19 +2,21 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
-
+gui.hide()
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
 /**
  * Textures
  */
