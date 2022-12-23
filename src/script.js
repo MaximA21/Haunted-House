1ba5f5a87dd29a93e48f33a0cd4455b6dc2bfae1
Maxim <118841284+MaximA21@users.noreply.github.com>
2022-12-23 17:25:53 +0100

    type

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ const textureLoader = new THREE.TextureLoader()
 
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientocclusion.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
 const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
